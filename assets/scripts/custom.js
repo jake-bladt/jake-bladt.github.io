a363ca1bd922aee323309e866c1ad95b51b89dd0
Jake <jake@jakebladt.com>
2016-02-19 22:33:32 -0500

    +replace with partial

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,3 +1,18 @@
 (function($) {
+  var app = function() {};
+
+  app.replaceWithPartial = function(selector, partialPageUrl) {
+  	var $elem = $(selector);
+  	$.get({ 
+	  url: partialPageUrl,
+	  done: function(html) {
+        $elem.replaceWith(html);
+	  });
+  }
+
+  $('#contactLink').click(function() {
+    app.replaceWithPartial('#mainContent', 'contact.html');
+  });
+
 	
 })(jQuery);
